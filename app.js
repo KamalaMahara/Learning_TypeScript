8e6ce2effb4ba9ec8e7b09eeceaf01f47bdc4960
Kamala Mahara <kmlamahara@gmail.com>
2025-12-30 22:44:43 +0545

    added datatypes

--- app.js
+++ app.js
@@ -1,4 +1,40 @@
-var subj = "hello world";
+// first program in ts
+var name = "hello world";
 var age = 43;
-console.log(subj);
+console.log(name);
 console.log(age);
+//string
+var firstname = "kmla";
+var lastname = "mahara";
+var fullname = "".concat(firstname, " ").concat(lastname);
+console.log(fullname);
+//boolean
+var isLoggedIn = true;
+console.log(isLoggedIn);
+//primitive types 
+var num = 10;
+var b = num;
+b = 20;
+console.log(num);
+console.log(b);
+//reference types
+var person = {
+    name: "kmla",
+    age: 54
+};
+var emp = person;
+emp.age = 80;
+console.log(person); // age will be 80  cause both person and emp refer to the same object memory location
+console.log(emp);
+//array
+var fruits = ["apple", "banana", "litchi", "dragoon"];
+console.log(fruits);
+var numberArray = [1, 2, 4, 56, 7];
+console.log(numberArray);
+var mixedArray = ["apple", 23, true, "litchi", 54, false];
+console.log(mixedArray);
+//tuple
+var role = [1, "admin"];
+console.log(role); //fixed length and fixed data types
+role.push("user");
+console.log(role);
--- app.ts
+++ app.ts
@@ -4,7 +4,49 @@ let age: number = 43;
 console.log(name);
 console.log(age);
 
-//basic types in ts
-//number
-let num1: number = 80;
-let num2 = 7;
+//string
+let firstname: string = "kmla";
+let lastname: string = "mahara";
+let fullname: string = `${firstname} ${lastname}`;
+console.log(fullname);
+
+//boolean
+let isLoggedIn: boolean = true;
+console.log(isLoggedIn);
+
+
+//primitive types 
+let num: number = 10;
+let b = num;
+b = 20;
+console.log(num);
+console.log(b);
+
+//reference types
+let person = {
+  name: "kmla",
+  age: 54
+}
+let emp = person;
+emp.age = 80;
+console.log(person);  // age will be 80  cause both person and emp refer to the same object memory location
+console.log(emp);
+
+//array
+let fruits: string[] = ["apple", "banana", "litchi", "dragoon"];
+console.log(fruits);
+
+let numberArray: number[] = [1, 2, 4, 56, 7];
+console.log(numberArray);
+
+let mixedArray: (string | number | boolean)[] = ["apple", 23, true, "litchi", 54, false];
+console.log(mixedArray);
+
+//tuple
+let role: [number, string] = [1, "admin"];
+console.log(role); //fixed length and fixed data types
+
+role.push("user");
+console.log(role); //but not recommended
+
+
